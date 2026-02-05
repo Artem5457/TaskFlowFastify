4bac9b479e07663583f12dacc220323560e9b9b6
Artem Bolshak <fcdnipro20152705@gmail.com>
2026-02-05 14:45:39 +0200

    Shutdown logic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { buildServer } from './server.js';
+import { setupShutdown } from './shared/utils/shutdown.js';
 
 const server = buildServer();
 
@@ -9,6 +10,8 @@ const bootstrap = async () => {
 
     const { port } = server.config;
     await server.listen({ port });
+
+    setupShutdown(server);
   } catch (err) {
     server.log.fatal({ err }, 'Failed to start the application');
     process.exit(1);
